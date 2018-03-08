8b13be1ad4b9ee382a3f2ccd964d0e5c46c782bd
Yufan Zheng <yufan.zheng1@gmail.com>
2018-03-08 17:35:58 +0800

    simple 2

--- coroutine/coroutine_simple.py
+++ coroutine/coroutine_simple.py
@@ -1,12 +1,53 @@
 def simple_coroutine():
-    """ A simplest corotine example
+    """ A simplest coroutine example
 
     >>> my_coro = simple_coroutine()
-    >>> print(my_coro)
-    >>> next(my_coro)                       # Execute to yield
-    >>> my_coro.send(42)                    # Will trigger a Stop Iteration Error
+    >>> my_coro
+    <generator object simple_coroutine at 0x104669240>
+    >>> next(my_coro)
+    -> Coroutine started
+    >>> my_coro.send(42)
+    -> Coroutine received: 42
+    Traceback (most recent call last): # ➏
+        ...
+    StopIteration
 
+    >>> my_coro = simple_coroutine()
+         >>> my_coro.send(1729)
+         Traceback (most recent call last):
+           File "<stdin>", line 1, in <module>
+         TypeError: can't send non-None value to a just-started generator
     """
     print('Coroutine started')
-    x = yield                           # Wait for a value from send()
-    print('-> coroutine recieved:', x)
+    while True:
+        x = yield                           # Wait for a value from send()
+        print('-> coroutine recieved:', x)
+
+
+def simple_coroutine2(a):
+    """ Coroutine with 2 values
+
+    >>> my_coro2 = simple_coro2(14)
+         >>> from inspect import getgeneratorstate
+    >>> getgeneratorstate(my_coro2)
+    'GEN_CREATED'
+    >>> next(my_coro2)
+    -> Started: a = 14
+    14
+    >>> getgeneratorstate(my_coro2)
+    'GEN_SUSPENDED'
+    >>> my_coro2.send(28)
+    -> Received: b = 28
+    42
+    >>> my_coro2.send(99)
+    -> Received: c = 99
+    Traceback (most recent call last):
+    File "<stdin>", line 1, in <module> StopIteration
+    >>> getgeneratorstate(my_coro2)
+    'GEN_CLOSED'
+    """
+    print('-> Started: a =', a)
+    b = yield a
+    print('-> Received: b =', b)
+    c = yield a + b
+    print('-> Received: c =', c)
