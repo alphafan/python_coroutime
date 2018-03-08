2546e664818145c17f1858c2a0f4df57cac78599
Yufan Zheng <yufan.zheng1@gmail.com>
2018-03-08 17:37:00 +0800

    fix

--- coroutine/coroutine_simple.py
+++ coroutine/coroutine_simple.py
@@ -13,10 +13,10 @@ def simple_coroutine():
     StopIteration
 
     >>> my_coro = simple_coroutine()
-         >>> my_coro.send(1729)
-         Traceback (most recent call last):
-           File "<stdin>", line 1, in <module>
-         TypeError: can't send non-None value to a just-started generator
+    >>> my_coro.send(1729)
+     Traceback (most recent call last):
+       File "<stdin>", line 1, in <module>
+     TypeError: can't send non-None value to a just-started generator
     """
     print('Coroutine started')
     while True:
@@ -27,8 +27,8 @@ def simple_coroutine():
 def simple_coroutine2(a):
     """ Coroutine with 2 values
 
-    >>> my_coro2 = simple_coro2(14)
-         >>> from inspect import getgeneratorstate
+    >>> my_coro2 = simple_coroutine2(14)
+    >>> from inspect import getgeneratorstate
     >>> getgeneratorstate(my_coro2)
     'GEN_CREATED'
     >>> next(my_coro2)
