c22f427532f47194094307aacb073df5c6dc8f8d
Yufan Zheng <yufan.zheng1@gmail.com>
2018-03-08 16:56:49 +0800

    change to doc test

--- coroutine/coroutine_simple.py
+++ coroutine/coroutine_simple.py
@@ -1,11 +1,12 @@
 def simple_coroutine():
+    """ A simplest corotine example
+
+    >>> my_coro = simple_coroutine()
+    >>> print(my_coro)
+    >>> next(my_coro)                       # Execute to yield
+    >>> my_coro.send(42)                    # Will trigger a Stop Iteration Error
+
+    """
     print('Coroutine started')
-    x = yield           # Wait for a value from send()
+    x = yield                           # Wait for a value from send()
     print('-> coroutine recieved:', x)
-
-
-if __name__ == '__main__':
-    my_coro = simple_coroutine()
-    print(my_coro)
-    next(my_coro)       # Execute to yield
-    my_coro.send(42)    # Will trigger a Stop Iteration Error
